2046e623a98c4746d85e33609c5abb5ce7d98280
jeboo <jeb00o@yahoo.com>
2013-01-12 21:44:42 -0500

    update mk.sh to play nice with initramfs git

--- mk.sh
+++ mk.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export CROSS_COMPILE=/home/none/android/toolchains/android-toolchain-eabi/bin/arm-eabi-
 
+find ../initramfs -name ".git*" -exec rm -rf {} \;
+find ../initramfs -name EMPTY_DIRECTORY -exec rm -rf {} \;
 find -name '*.ko' -exec cp -av {} ../initramfs/lib/modules/ \;
 chmod 644 ../initramfs/lib/modules/*
 ${CROSS_COMPILE}strip --strip-unneeded ../initramfs/lib/modules/*
